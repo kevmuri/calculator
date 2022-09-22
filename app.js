47e80a61c3fd1be2dc5bd81a91631cb80f75fe95
kevmuri <kevmuri14@gmail.com>
2022-09-22 16:42:33 -0400

    add js to populate screen with digits

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 let num1 = null;
 let num2 = null;
+const numButtons = document.getElementsByClassName("number");
+
+for (let i = 0; i < numButtons.length; i++) {
+    numButtons[i].addEventListener("click", function(){appendNumber(this.innerHTML)});
+}
+
+function appendNumber (value) {
+    let display = document.getElementById("display")
+    if (display.innerHTML == "0") {
+        display.innerHTML= "";
+    }
+    display.innerHTML += value;
+}
