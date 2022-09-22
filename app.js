38fbd024bd6572d933767ae0e4208d64bccca8a3
kevmuri <kevmuri14@gmail.com>
2022-09-22 16:49:59 -0400

    introduce clear functionality

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let num1 = null;
 let num2 = null;
 const numButtons = document.getElementsByClassName("number");
 
+document.getElementById("clear").addEventListener("click", function(){clear()});
+
 for (let i = 0; i < numButtons.length; i++) {
     numButtons[i].addEventListener("click", function(){appendNumber(this.innerHTML)});
 }
@@ -12,4 +14,10 @@ function appendNumber (value) {
         display.innerHTML= "";
     }
     display.innerHTML += value;
+}
+
+function clear () {
+    document.getElementById("display").innerHTML = "0";
+    num1 = null;
+    num2 = null;
 }
