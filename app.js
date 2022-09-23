197037c269f948d63ebb77de2db006a53d860ba5
kevmuri <kevmuri14@gmail.com>
2022-09-23 15:05:34 -0400

    messy code, working solution

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 let num1 = null;
-let num2 = null;
+let operator = null;
+const display = document.getElementById("display")
 const numButtons = document.getElementsByClassName("number");
+const operators = document.getElementsByClassName("operator");
 
 document.getElementById("clear").addEventListener("click", function(){clear()});
+document.getElementById("operate").addEventListener("click", function(){operate()});
+
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", function(){setOperator(this.innerHTML)});
+}
 
 for (let i = 0; i < numButtons.length; i++) {
     numButtons[i].addEventListener("click", function(){appendNumber(this.innerHTML)});
 }
 
 function appendNumber (value) {
-    let display = document.getElementById("display")
     if (display.innerHTML == "0") {
         display.innerHTML= "";
     }
@@ -19,5 +25,50 @@ function appendNumber (value) {
 function clear () {
     document.getElementById("display").innerHTML = "0";
     num1 = null;
-    num2 = null;
+    operator = null;
+}
+
+function setOperator (value) {
+    switch (value) {
+        case "+":
+            operator = "add";
+            num1 = Number(document.getElementById("display").innerHTML);
+            document.getElementById("display").innerHTML = "0";
+            break;
+        case "-":
+            operator = "subtract";
+            num1 = Number(document.getElementById("display").innerHTML);
+            document.getElementById("display").innerHTML = "0";
+            break;
+        case "x":
+            operator = "multiply";
+            num1 = Number(document.getElementById("display").innerHTML);
+            document.getElementById("display").innerHTML = "0";
+            break;
+        case "/":
+            operator = "divide";
+            num1 = Number(document.getElementById("display").innerHTML);
+            document.getElementById("display").innerHTML = "0";
+            break;
+    }
+}
+
+function operate () {
+    console.log(operator)
+    switch (operator) {
+        case "add":
+            document.getElementById("display").innerHTML = num1 + Number(document.getElementById("display").innerHTML);
+            break;
+        case "subtract":
+            document.getElementById("display").innerHTML = num1 - Number(document.getElementById("display").innerHTML);
+            break;
+        case "multiply":
+            document.getElementById("display").innerHTML = num1 * Number(document.getElementById("display").innerHTML);
+            break;
+        case "divide":
+
+
+            document.getElementById("display").innerHTML = num1 / Number(document.getElementById("display").innerHTML);
+            break;
+    }
 }
